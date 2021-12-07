f28ea9033bd9740019c9138343b8e195c9f08ea3
ak0513 <minimar0812@gmail.com>
2021-12-07 16:16:34 +0900

    aa

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,147 +1,83 @@
 <template>
 	<div id="app">
-		<router-link to="/insurance/about">about</router-link>
-		<router-link to="/insurance/home">home</router-link>	
-		<router-link to="/insurance/">index</router-link>	
-		<router-view></router-view>
-		
-		<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-
-		<div class="container">
-			<div class="row">
-				<div class="col">
-					<button type="button" class="btn btn-secondary btn-type-all" data-type="data-all" @click="sort">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-all" viewBox="0 0 16 16">
-							<path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z"/>
-						</svg>
-						All View
-					</button>
-					<button type="button" class="btn btn-primary" data-type="data-a" @click="sort">Type-A</button>
-					<button type="button" class="btn btn-success" data-type="data-b" @click="sort">Type-B</button>
-					<button type="button" class="btn btn-danger" data-type="data-c" @click="sort">Type-C</button>
-					<button type="button" class="btn btn-info" data-type="data-d" @click="sort">Type-D</button>
-
-					<div class="mt-4">
-						<ul class="item-list row p-0 g-2">
-							<li class="data-a col-12 col-md-6">
-								<b-card 
-									title="Card Title"
-									header="Primary"
-									header-bg-variant="primary"
-									header-text-variant="white"
-									footer="footer1"
-									footer-text-variant="primary"
-								>
-									<b-card-text>
-										This is a wider card with supporting text below as a natural lead-in to additional content.
-										This content is a little bit longer.
-									</b-card-text>
-									<template #header>
-										<small class="">header</small>
-									</template>
-									<template #footer>
-										<small class="">footer</small>
-									</template>
-								</b-card>
-							</li>
-							<li class="data-b col-4"><div>2B</div></li>
-							<li class="data-a col-4"><div>3A</div></li>
-							<li class="data-b col-4"><div>4B</div></li>
-							<li class="data-b col-4"><div>5B</div></li>
-							<li class="data-a col-4"><div>6A</div></li>
-							<li class="data-c col-4"><div>7C</div></li>
-							<li class="data-c col-4"><div>8C</div></li>
-							<li class="data-a col-4"><div>9A</div></li>
-							<li class="data-a col-4"><div>10A</div></li>
-							<li class="data-c col-4"><div>11C</div></li>
-							<li class="data-d col-4"><div>12D</div></li>
-						</ul>
+		<Header></Header>
+		<div class="val">{{ val2 }}</div>
+		<div class="itemTitle">{{ itemTitle }}</div>
+		<main id="main">
+			<div class="container">
+				<router-view></router-view>
+				<div class="row">
+					<div class="col">
+						<ItemList></ItemList>
 					</div>
 				</div>
 			</div>
-		</div>
+		</main>
 		
-		<div id="item1">
-			<item1 msg="item1" />
-		</div>
-		
-		<b-button v-b-modal.modal-1>Launch demo modal</b-button>
-		<modal1/>
-
+		<child message="부모에서 자식으로 데이터 넘겨주기" @callParent="setTxt2"></child>
+		<AsideNav @getItemTitle="setTxt"></AsideNav>
 	</div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import item1 from './components/item1.vue'
-import modal1 from './components/modal1.vue'
+import Header from './components/header.vue'
+import AsideNav from './components/asideNav.vue'
+import ItemList from './components/item-list.vue'
+import child from './components/child.vue'
 
 export default {
 	name: 'App',
 	components: {
-		// HelloWorld,
-		item1,
-		modal1
+		AsideNav,
+		ItemList,
+		child,
+		Header
 	},
+	data:() => ({
+		val: '',
+		val2: '',
+		aaa: 'aaa',
+		itemTitle: 'All View'
+	}),
 	methods: {
-		sort(e) {
-			let ele = e.target;
-			let eleDataType = ele.getAttribute('data-type');
-			let dataType = document.querySelectorAll('.' + ele.getAttribute('data-type'));
-
-			let itemList = document.querySelector('.item-list');
-			let dataAll = document.querySelectorAll('.item-list > li');
-
-			if(!itemList.classList.contains('sort')) {
-				itemList.classList.add('sort');
-			}
-
-			if(eleDataType === 'data-all') {
-				itemList.classList.remove('sort');
-			} else {
-				dataAll.forEach(function(e) {
-					e.classList.remove('active')
-				})
-				dataType.forEach(function(e) {
-					e.classList.add('active')
-				})
-			}
+		setTxt2(title2) {
+			this.val2 = title2;
+			console.log('자식에게 값을 받았어요' , this.val2)
+		},
+		setTxt(title) {
+			this.itemTitle = title;
+			console.log('자식에게 값을 받았어요' , this.itemTitle)
+		},
+		setChange2() {
+			console.log(this.aaa)
+			this.aaa = 'bbb'
+			console.log(this.aaa)
 		}
 	}
 }
 </script>
 
 <style>
+body,
+html {
+	height:100%;
+}
+body {
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji", sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	display:flex;
+	flex-direction: column;
+	min-height:100%;
+
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #212731;
+	background-color: #e8eaef;
 }
 
-ul.item-list li {
-	list-style: none;
-}
-
-ul.item-list.sort li {
-	display:none;
-}
-ul.item-list.sort li.active {
-	display:block;
-}
-
-.card-deck {
-	display: flex;
-    flex-flow: row wrap;
-    margin-right: -15px;
-    margin-left: -15px;
-}
-.card-deck .card {
-	flex: 1 0 0%;
-    margin-right: 15px;
-    margin-bottom: 0;
-    margin-left: 15px;
+#main {
+	flex-grow: 1;
 }
 </style>
---
+++ vue/src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
+</template>
+
+<script>
+export default {
+  name: 'header-components'
+}
+</script>
+
+<style scoped>
+
+</style>
